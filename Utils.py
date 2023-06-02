deb5f5d3fdc77d0edb03fd83c6a185ac7d7ccb3d
Koustav Bose <92668707+code4koustav@users.noreply.github.com>
2023-06-02 17:52:50 +0530

    Final changes in code

--- Utils.py
+++ Utils.py
@@ -3,24 +3,83 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+from tqdm import tqdm
+import matplotlib as plt
 
-class Net(nn.Module):
-    #This defines the structure of the NN.
+class Processing(tqdm,plt):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
-        self.fc1 = nn.Linear(4096, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.train_losses = []
+        self.test_losses = []
+        self.train_acc = []
+        self.test_acc = []
+        self.tqdm = tqdm()
 
-    def forward(self, x):
-        x = F.relu(self.conv1(x), 2) # 28>26 | 1>3 | 1>1
-        x = F.relu(F.max_pool2d(self.conv2(x), 2)) #26>24>12 | 3>5>6 | 1>1>2
-        x = F.relu(self.conv3(x), 2) # 12>10 | 6>10 | 2>2
-        x = F.relu(F.max_pool2d(self.conv4(x), 2)) # 10>8>4 | 10>14>16 | 2>2>4
-        x = x.view(-1, 4096) # 4*4*256 = 4096
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+    def GetCorrectPredCount(self, pPrediction, pLabels):
+        return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+    def train(self, model,device,train_loader,optimizer,criterion):
+
+        model.train()
+        pbar = tqdm(train_loader)
+        train_loss = 0
+        correct = 0
+        processed = 0
+
+        for batch_idx, (data, target) in enumerate(pbar):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+
+            # Predict
+            pred = model(data)
+
+            # Calculate loss
+            loss = criterion(pred, target)
+            train_loss+=loss.item()
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+            
+            correct += self.GetCorrectPredCount(pred, target)
+            processed += len(data)
+
+            pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+        self.train_acc.append(100*correct/processed)
+        self.train_losses.append(train_loss/len(train_loader))
+
+    def test(self,model,device,test_loader,criterion):
+        model.eval()
+
+        test_loss = 0
+        correct = 0
+
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(test_loader):
+                data, target = data.to(device), target.to(device)
+
+                output = model(data)
+                test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+
+                correct += self.GetCorrectPredCount(output, target)
+
+
+        test_loss /= len(test_loader.dataset)
+        self.test_acc.append(100. * correct / len(test_loader.dataset))
+        self.test_losses.append(test_loss)
+
+        print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+    
+    def result(self):
+
+        fig, axs = plt.subplots(2,2,figsize=(15,10))
+        axs[0, 0].plot(self.train_losses)
+        axs[0, 0].set_title("Training Loss")
+        axs[1, 0].plot(self.train_acc)
+        axs[1, 0].set_title("Training Accuracy")
+        axs[0, 1].plot(self.test_losses)
+        axs[0, 1].set_title("Test Loss")
+        axs[1, 1].plot(self.test_acc)
+        axs[1, 1].set_title("Test Accuracy")
